138d0bc03674005f2a4a450ec4e5767322c81bdb
MohonSaha <practicewithmohon@gmail.com>
2023-05-16 18:17:18 +0600

    update doctor data through patch

--- index.js
+++ index.js
@@ -36,92 +36,136 @@ async function run() {
 
 
     //Read or show all data of services:-
-    app.get('/allServices', async(req, res) =>{
-        const cursor = servicesCollection.find()
-        const result = await cursor.toArray();
-        res.send(result)
+    app.get('/allServices', async (req, res) => {
+      const cursor = servicesCollection.find()
+      const result = await cursor.toArray();
+      res.send(result)
     })
-    
 
-    //Read or show all data of doctors:-
-    app.get('/allDoctors', async(req, res) =>{
+
+    //Read or show data according to pagination of doctors:-
+    app.get('/allDoctors', async (req, res) => {
       // console.log(req.query);
       const page = parseInt(req.query.page) || 0;
       const limit = parseInt(req.query.limit) || 4;
       const skip = page * limit;
 
-
       const result = await doctorsCollection.find().skip(skip).limit(limit).toArray();
       res.send(result)
-  })
+    })
+
+
+    //Read or show all data of doctors:-
+    app.get('/allDoctorsData', async (req, res) => {
+      const result = await doctorsCollection.find().toArray();
+      res.send(result)
+    })
 
 
     //Read or show total number of data of doctors:-
-    app.get('/totalDoctors', async(req, res) =>{
-      
+    app.get('/totalDoctors', async (req, res) => {
+
       const result = await doctorsCollection.estimatedDocumentCount();
-      res.send({totalDoctors: result})
+      res.send({ totalDoctors: result })
     })
 
 
 
 
     // Read or show specific data :-
-    app.get('/singleServices/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await servicesCollection.findOne(query);
-        res.send(result)
-    })
-
-    // Read or show specific data :-
-    app.get('/doctorsDetails/:id', async(req, res) =>{
+    app.get('/singleServices/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
+      const result = await servicesCollection.findOne(query);
+      res.send(result)
+    })
+
+    // Read or show specific dcotor data :-
+    app.get('/doctorsDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
       const result = await doctorsCollection.findOne(query);
       res.send(result)
-  })
-
-
-    // Create or add or insert new data:- 
-    app.post('/addServices', async(req, res)=> {
-        const newServices = req.body;
-        const result = await servicesCollection.insertOne(newServices)
-        res.send(result)
     })
 
 
-    // Update existing data :-
+    // Create or add or insert new services data:- 
+    app.post('/addServices', async (req, res) => {
+      const newServices = req.body;
+      const result = await servicesCollection.insertOne(newServices)
+      res.send(result)
+    })
+
+    // Create or add or insert new doctors data:-
+    app.post('/addDoctors', async (req, res) => {
+      const newDoctors = req.body;
+      const result = await doctorsCollection.insertOne(newDoctors)
+      res.send(result)
+    })
+
+
+
+    // Update existing services data :-
 
     // Receive data from UI:-
-    app.put('/updateService/:id', async(req, res)=>{
-        const id = req.params.id;
-        const service = req.body;
+    app.put('/updateService/:id', async (req, res) => {
+      const id = req.params.id;
+      const service = req.body;
 
-
-        //send update data to the database:-
-        const filter = {_id: new ObjectId(id)};
-        const options = {upsert: true};
-        const updatedService = {
-            $set: {
-                name: service.name,
-                photo: service.photo,
-                details: service.details
-            }
+      //send update data to the database:-
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedService = {
+        $set: {
+          name: service.name,
+          photo: service.photo,
+          details: service.details
         }
+      }
 
-        const result = await servicesCollection.updateOne(filter, updatedService, options);
-        res.send(result);
+      const result = await servicesCollection.updateOne(filter, updatedService, options);
+      res.send(result);
+    })
+
+    // Update existing services data:-
+    app.patch('/updateDoctor/:id', async (req, res) => {
+      const id = req.params.id;
+      const doctorData = req.body;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedDoctor = {
+        $set: {
+          name: doctorData.name,
+          photo: doctorData.photo,
+          details: doctorData.details,
+          speciality: doctorData.speciality,
+          degree: doctorData.speciality,
+          Wplace: doctorData.Wplace
+        }
+      }
+
+      const result = await doctorsCollection.updateOne(filter, updatedDoctor, options)
+      res.send(result)
     })
 
 
 
-    // Delete a specific data form database:-
-    app.delete('/remove/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await servicesCollection.deleteOne(query);
-        res.send(result)
+    // Delete a specific service data form database:-
+    app.delete('/remove/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result)
+    })
+
+
+    // Delete a doctor data form database:-
+    app.delete('/removeDoctor/:id', async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = { _id: new ObjectId(id) }
+      const result = await doctorsCollection.deleteOne(query);
+      res.send(result)
     })
 
 
@@ -141,11 +185,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('DocTalk Server is running...')
-  })
+  res.send('DocTalk Server is running...')
+})
 
 
 
-app.listen(port, ()=>{
-    console.log(`DocTalk is running on ${port}`);
+app.listen(port, () => {
+  console.log(`DocTalk is running on ${port}`);
 })
